24414b26b588b92016626d08c194d5a0800230ac
AddisonRogers <87341723+AddisonRogers@users.noreply.github.com>
2022-01-24 02:22:16 +0000

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Validate
+{
+    public class Validate
+    {
+        private int Valint(_num, ) //input a console.readline and it will output a int
+        {
+            bool _run = true;
+            while (_run)
+            {
+                if (try(Convert.ToInt32){ })
+                    
+            }
+            
+        }
+
+        
+    }
+}
